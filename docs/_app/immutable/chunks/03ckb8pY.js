6fe8d828c687a651cff6af4c779ab31d4cf59d4f
Sahil Ramjeeth <22046468@dut4life.ac.za>
2025-11-25 18:24:55 +0200

    GSC101

--- docs/_app/immutable/chunks/03ckb8pY.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_eg8fme?.base??"/faq-schema-generator",e=globalThis.__sveltekit_eg8fme?.assets??s??"";export{e as a,s as b};
---
+++ docs/_app/immutable/chunks/uGmy9XF0.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_14fonnc?.base??"/faq-schema-generator",a=globalThis.__sveltekit_14fonnc?.assets??s??"";export{a,s as b};
